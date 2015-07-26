5309d4576f7e85e94a24de5892f9d5d6f2430ea9
Josh <josh.quintus@gmail.com>
2015-07-26 19:36:43 -0400

    Improve error reporting. Fixes #1

--- TidyJson/TidyJson/Options.cs
+++ TidyJson/TidyJson/Options.cs
@@ -8,12 +8,18 @@ namespace TidyJson
         [Option('o', "output", Required = false, HelpText = "File to save the ouput")]
         public string OutputFile { get; set; }
 
-        [Option('c', 
-            "clipboard", 
-            Required = false, 
+        [Option('c',
+            "clipboard",
+            Required = false,
             MutuallyExclusiveSet = "input",
             HelpText = "Read input from the clipboard and write output back out to the clipboard")]
         public bool Clipboard { get; set; }
+
+        [Option('v',
+            "verbose",
+            Required = false,
+            HelpText = "Include detailed information in the output")]
+        public bool Verbose { get; set; }
 
         public string GetUsage()
         {
--- TidyJson/TidyJson/Program.cs
+++ TidyJson/TidyJson/Program.cs
@@ -8,18 +8,36 @@ namespace TidyJson
         private static int Main(string[] args)
         {
             var options = new Options();
-            if (!CommandLine.Parser.Default.ParseArguments(args, options))
+            try
             {
-                var usage = options.GetUsage();
-                Console.WriteLine(usage);
 
+                if (!CommandLine.Parser.Default.ParseArguments(args, options))
+                {
+                    var usage = options.GetUsage();
+                    Console.WriteLine(usage);
+
+                    return -1;
+                }
+
+                var factory = new StreamFactory(options);
+                var cleaner = new Cleaner(factory);
+
+                cleaner.Clean();
+            }
+            catch (Exception ex)
+            {
+
+                Console.Error.WriteLine("Error formatting JSON");
+                if (options.Verbose)
+                {
+                    Console.Error.WriteLine(ex);
+                }
+                else
+                {
+                    Console.Error.WriteLine(ex.Message);
+                }
                 return -1;
             }
-
-            var factory = new StreamFactory(options);
-            var cleaner = new Cleaner(factory);
-
-            cleaner.Clean();
 
             return 0;
         }
