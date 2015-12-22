58d6c0d958ecbabb6014d2e5c21e8dba23deb3d6
Josh <josh.quintus@gmail.com>
2015-12-22 16:07:10 -0500

    remove try/catch blocks
    in response to #6

--- TidyJson/TidyJson/Chocolatey/Tools/chocolateyInstall.ps1
+++ TidyJson/TidyJson/Chocolatey/Tools/chocolateyInstall.ps1
@@ -1,17 +1,9 @@
 ﻿$packageName = 'TidyJson.portable'
 $url ="https://github.com/MasterDevs/TidyJson/releases/download/v1.0.1/bin.zip"
 
-try 
-{
-  $installDir = Join-Path $env:AllUsersProfile "$packageName"
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  Install-ChocolateyPath "$installDir"
-  $env:Path = "$($env:Path);$installDir"
+$installDir = Join-Path $env:AllUsersProfile "$packageName"
+Write-Host "Adding `'$installDir`' to the path and the current shell path"
+Install-ChocolateyPath "$installDir"
+$env:Path = "$($env:Path);$installDir"
 
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
-} 
-catch 
-{
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
