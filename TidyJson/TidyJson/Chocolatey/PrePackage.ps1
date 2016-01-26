b9134a6d89d137cbaebe33ad79118a5497a4f937
Josh <josh.quintus@gmail.com>
2016-01-25 23:09:52 -0500

    Create a powershell script to update the choco (un)install scripts

---
+++ TidyJson/TidyJson/Chocolatey/PrePackage.ps1
@@ -0,0 +1,15 @@
+﻿$versionToken = "__VERSION__";
+$version = "v1.0.1"
+
+$packageNameToken = "__PACKAGE_NAME__";
+$packageName = "TidyJson.portable";
+
+$projectBaseUrl = "https://github.com/MasterDevs/TidyJson";
+$projectBaseUrlToken = "__PROJECT_BASE_URL__"
+
+Get-ChildItem -r -include "chocolatey*.ps1","*.nuspec" |
+ ForEach-Object { $file = $_; ( Get-Content $file ) |
+ ForEach-Object { $_ -replace $versionToken, $version }  | 
+ ForEach-Object { $_ -replace $packageNameToken, $packageName }  | 
+ ForEach-Object { $_ -replace $projectBaseUrlToken, $projectBaseUrl }  | 
+Set-Content $file.FullName  }
--- TidyJson/TidyJson/Chocolatey/Tools/chocolateyInstall.ps1
+++ TidyJson/TidyJson/Chocolatey/Tools/chocolateyInstall.ps1
@@ -1,17 +1,9 @@
-﻿$packageName = 'TidyJson.portable' # arbitrary name for the package, used in messages
-$url ="https://github.com/MasterDevs/TidyJson/releases/download/v0.0.1/bin.zip"
+﻿$packageName = '__PACKAGE_NAME__' # arbitrary name for the package, used in messages
+$url ="__PROJECT_BASE_URL__/releases/download/__VERSION__/bin.zip"
 
-try 
-{
-  $installDir = Join-Path $env:AllUsersProfile "$packageName"
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  Install-ChocolateyPath "$installDir"
-  $env:Path = "$($env:Path);$installDir"
+$installDir = Join-Path $env:AllUsersProfile "$packageName"
+Write-Host "Adding `'$installDir`' to the path and the current shell path"
+Install-ChocolateyPath "$installDir"
+$env:Path = "$($env:Path);$installDir"
 
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
-} 
-catch 
-{
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
