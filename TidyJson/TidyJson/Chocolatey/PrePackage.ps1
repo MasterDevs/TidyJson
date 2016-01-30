2bddcdb58f512cc2630ea219418fa98398e8124a
Josh <josh.quintus@gmail.com>
2016-01-29 23:47:16 -0500

    Move tokens to a dedicateed file.
    Also added a bunch of comments.

--- TidyJson/TidyJson/Chocolatey/PrePackage.ps1
+++ TidyJson/TidyJson/Chocolatey/PrePackage.ps1
@@ -1,25 +1,58 @@
-﻿$versionToken = "__VERSION__";
+﻿################################################################################
+# Prepackage.ps1
+#
+# This script generates the chocolatey install and uninstall powershell scripts 
+# found in the Tools directory in the chocolatey package.  
+#
+# It reads template files fond in the ToolsTemplates directory and replaces 
+# tokens with run time values.  Tokens are typically strings with leading and 
+# trailing underscores, e.g. __PACKAGE_NAME__.
+#
+# Tokens are defined in the Tokens.Json file in the ToolsTemplates directory.
+# __VERSION__ is a special token, it is hardcoded to be replaced with the
+# environtment variable APPVEYOR_REPO_TAG_NAME.  This allows AppVeyor to 
+# update version information in the scripts
+#
+# For more information see:
+# https://github.com/MasterDevs/ChocolateyCoolWhip
+################################################################################
+
+$versionToken = "__VERSION__";
 $version = $env:APPVEYOR_REPO_TAG_NAME
 Write-Host "Starting prepackage of version $version";
 
-$json = @"
-  [
-    { Name: "__PACKAGE_NAME__",     Value: "TidyJson.portable" },
-    { Name: "__PROJECT_BASE_URL__", Value: "https://github.com/MasterDevs/TidyJson" }
-  ]
-"@;
-
-$tokens = ConvertFrom-Json $json
-
+#######################################
+# Find Our Directories
+#######################################
+Write-Host
 $templateDirectory = Get-ChildItem -Recurse -Depth 10 -Directory -Include ToolsTemplates
 
 $toolsDir = "$($templateDirectory.Parent.FullName)\Tools"
 write-host "Creating $toolsDir"
 $silent = New-Item -ItemType Directory -Force -Path $toolsDir
 
+#######################################
+# Read Tokens
+#######################################
 Write-Host
+Write-Host "Reading tokens"
 
-foreach($file in $templateDirectory.EnumerateFiles())
+$tokenFile = $templateDirectory.EnumerateFiles().Where({$PSItem.Name -eq "tokens.json";})[0]
+$json = Get-Content -Raw $tokenFile.FullName
+$tokens = ConvertFrom-Json $json
+Write-Host "Replacing tokens"
+Write-Host "    $versionToken ==> $version"
+foreach($token in $tokens)
+{
+    Write-Host "    $($token.Name) ==> $($token.Value)"
+}
+
+
+#######################################
+# Convert tokenized files
+#######################################$con
+Write-Host
+foreach($file in $templateDirectory.EnumerateFiles().Where({$PSItem.Name -ne "tokens.json";}))
 {
     Write-Host "Processing $($file.FullName)"
     $newFile = "$($toolsDir)\\$($file.Name)"
@@ -38,4 +71,7 @@ foreach($file in $templateDirectory.EnumerateFiles())
     Write-Host
 }
 
+#######################################
+# Done
+#######################################
 Write-Host "Prepackage complete"
